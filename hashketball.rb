5dddc5cff63e237557e075e925d80495418d293b
Kevin Park <kevin.park07@yahoo.com>
2020-07-18 01:21:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -135,7 +135,7 @@ end
 def num_points_scored(player)
  player_list = player_directory
   points_scored = ""
-  player_directory.each {|player, points|
+  player_list.each {|player, points|
     if player == player_directory[:player][:player_name]
      return player_directory[:player][:score]
     end

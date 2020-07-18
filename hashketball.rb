6094776dec5deae9a0386cdbaed49b43e1b1151e
Kevin Park <kevin.park07@yahoo.com>
2020-07-18 01:00:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,14 +130,16 @@ end
 def player_directory
 game_hash[:home][:players].merge(game_hash[:away][:players])
 end
-binding.pry 
+
 
 def num_points_scored(player)
   player_directory.each {
     if player = [:player][:player_name]
       player_directory[:player][:score]
+    end
+  }
 end
-  
+binding.pry 
   
   
   
